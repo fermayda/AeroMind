ccc191b2e90560ca9be1f9c054b82f33017e6abd
Eric Pedley <ericpedley@gmail.com>
2025-02-07 10:33:57 -0800

    connect LQR controller to gym environment

--- src/minimal_quadrotor_control.py
+++ src/minimal_quadrotor_control.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pydrake.all import (
-    DiagramBuilder, SceneGraph, Simulator, 
-    StartMeshcat, MeshcatVisualizer, ConstantVectorSource
+    DiagramBuilder, SceneGraph, Simulator, System, Context,
+    StartMeshcat, MeshcatVisualizer 
 )
 from pydrake.examples import QuadrotorPlant, QuadrotorGeometry, StabilizingLQRController
 from pydrake.gym._drake_gym_env import DrakeGymEnv
@@ -22,8 +22,10 @@ def quadrotor_example():
     # builder.Connect(controller.get_output_port(0), plant.get_input_port(0))
     # builder.Connect(plant.get_output_port(0), controller.get_input_port(0))
 
-    plant_input_port = builder.ExportInput(plant.get_input_port(0), "plant_input")
-    plant_output_port = builder.ExportOutput(plant.get_output_port(0), "state")
+    PLANT_INPUT_NAME = "plant_input"
+    PLANT_OUTPUT_NAME = "state"
+    plant_input_port = builder.ExportInput(plant.get_input_port(0), PLANT_INPUT_NAME)
+    plant_output_port = builder.ExportOutput(plant.get_output_port(0), PLANT_OUTPUT_NAME)
 
 
     # Set up visualization in MeshCat
@@ -40,10 +42,16 @@ def quadrotor_example():
     # Set up a simulator to run this diagram
     simulator = Simulator(diagram)
     simulator.set_target_realtime_rate(1.0)
-    context = simulator.get_mutable_context()
 
-    def reward_fn(system, context):
-        return -1
+    target_position = np.array([0, 0, 1])
+    target_velocity = np.array([0, 0, 0])
+
+    def reward_fn(system: System, context: Context):
+        state = system.get_output_port(0).Eval(context) 
+        # print(state.shape, state[:3])
+        pos_err = np.linalg.norm(state[:3] - target_position)
+        vel_err = np.linalg.norm(state[3:6] - target_velocity)
+        return -pos_err - vel_err
 
     gym_env = DrakeGymEnv(
         simulator, 
@@ -51,16 +59,25 @@ def quadrotor_example():
         gym.spaces.Box(low=0, high=100, shape=(4,)),
         gym.spaces.Box(low=-100, high=100, shape=(12,)),
         reward_fn,
-        'plant_input',
-        'state',
+        PLANT_INPUT_NAME,
+        PLANT_OUTPUT_NAME,
     )
+
+    dummy_plant = QuadrotorPlant()
+    controller = StabilizingLQRController(dummy_plant, target_position)
+    controller_context = controller.CreateDefaultContext()
 
     for _ in range(5):
         print("Starting episode")
         gym_env.reset()
+        action = np.zeros(4)
         for _ in tqdm(range(1000)):
-            output = gym_env.step(np.ones(4)*10)
-            print(output)
+            observation, reward, terminated, truncated, info = gym_env.step(action)
+            if terminated:
+                break
+            controller.get_input_port(0).FixValue(controller_context, observation)
+            action = controller.get_output_port(0).Eval(controller_context)
+        input("Press Enter to start the next episode...")
 
 
 quadrotor_example()
