a087af29b79014142de0a19d0d1aab5fae972d0d
Eric Pedley <ericpedley@gmail.com>
2025-02-07 10:20:43 -0800

    successfully wrap quadrotor with gym environment

--- src/minimal_quadrotor_control.py
+++ src/minimal_quadrotor_control.py
@@ -1,9 +1,12 @@
 import numpy as np
 from pydrake.all import (
     DiagramBuilder, SceneGraph, Simulator, 
-    StartMeshcat, MeshcatVisualizer
+    StartMeshcat, MeshcatVisualizer, ConstantVectorSource
 )
 from pydrake.examples import QuadrotorPlant, QuadrotorGeometry, StabilizingLQRController
+from pydrake.gym._drake_gym_env import DrakeGymEnv
+import gymnasium as gym
+from tqdm import tqdm
 
 # Quadrotor LQR example notebook:
 # https://deepnote.com/workspace/Underactuated-2ed1518a-973b-4145-bd62-1768b49956a8/project/096cffe7-416e-4d51-a471-5fd526ec8fab/notebook/quadrotor-ac8fa093ca5a4894a618ada316f2750b
@@ -15,9 +18,13 @@ def quadrotor_example():
 
     plant = builder.AddSystem(QuadrotorPlant())
 
-    controller = builder.AddSystem(StabilizingLQRController(plant, [0, 0, 1]))
-    builder.Connect(controller.get_output_port(0), plant.get_input_port(0))
-    builder.Connect(plant.get_output_port(0), controller.get_input_port(0))
+    # controller = builder.AddSystem(StabilizingLQRController(plant, [0, 0, 1]))
+    # builder.Connect(controller.get_output_port(0), plant.get_input_port(0))
+    # builder.Connect(plant.get_output_port(0), controller.get_input_port(0))
+
+    plant_input_port = builder.ExportInput(plant.get_input_port(0), "plant_input")
+    plant_output_port = builder.ExportOutput(plant.get_output_port(0), "state")
+
 
     # Set up visualization in MeshCat
     scene_graph = builder.AddSystem(SceneGraph())
@@ -35,17 +42,25 @@ def quadrotor_example():
     simulator.set_target_realtime_rate(1.0)
     context = simulator.get_mutable_context()
 
-    # Simulate
-    for i in range(5):
-        context.SetTime(0.0)
-        context.SetContinuousState(
-            0.5
-            * np.random.randn(
-                12,
-            )
-        )
-        simulator.Initialize()
-        simulator.AdvanceTo(4.0)
+    def reward_fn(system, context):
+        return -1
+
+    gym_env = DrakeGymEnv(
+        simulator, 
+        0.001, 
+        gym.spaces.Box(low=0, high=100, shape=(4,)),
+        gym.spaces.Box(low=-100, high=100, shape=(12,)),
+        reward_fn,
+        'plant_input',
+        'state',
+    )
+
+    for _ in range(5):
+        print("Starting episode")
+        gym_env.reset()
+        for _ in tqdm(range(1000)):
+            output = gym_env.step(np.ones(4)*10)
+            print(output)
 
 
 quadrotor_example()
