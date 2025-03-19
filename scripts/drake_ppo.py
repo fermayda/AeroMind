6f1509c82627bcab9cf0d21d00bb7026a34aeafd
Eric Pedley <ericpedley@gmail.com>
2025-03-19 01:48:51 -0700

    add loading and saving to diff directories

--- scripts/drake_ppo.py
+++ scripts/drake_ppo.py
@@ -7,7 +7,16 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from datetime import datetime
 from cs175.quadrotor_env import QuadrotorEnv
+import argparse
 
+import argparse
+
+parser = argparse.ArgumentParser(description="Example script with --load flag")
+parser.add_argument('--load', nargs='?', const='default_value', help="Load with an optional string")
+
+args = parser.parse_args()
+
+load_val: str | None = args.load
 # Quadrotor LQR example notebook:
 # https://deepnote.com/workspace/Underactuated-2ed1518a-973b-4145-bd62-1768b49956a8/project/096cffe7-416e-4d51-a471-5fd526ec8fab/notebook/quadrotor-ac8fa093ca5a4894a618ada316f2750b
 
@@ -32,5 +41,7 @@ env = make_vec_env(make_env, n_envs=1)
 #                                 tb_log_dir=experiment_logdir)
 
 model = PPO("MlpPolicy", env, tensorboard_log=experiment_logdir, device='cpu')
+if load_val is not None:
+    model.load(load_val)
 model.learn(total_timesteps=1_000_000)
-model.save("ppo_quadrotor_stabilize_unvectorized")
+model.save(f'{experiment_logdir}/weights')
